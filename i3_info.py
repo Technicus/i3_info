6ccc6484da97c67346e4030fa0ad56773f223094
Technicus <>
2022-05-16 08:18:46 -0500

    Window, output, and key binding report working.

--- i3_info.py
+++ i3_info.py
@@ -16,11 +16,11 @@ def clear():
 
 
 # Define a callback to be called when you switch workspaces.
-def on_workspace_focus(i3, event):
+async def on_workspace_focus(i3, event):
     # The first parameter is the connection to the ipc and the second is an object
     # with the data of the event sent from i3.
     if event.current:
-        print(f"on_workspace_focus(i3, event):\n  {event}\n")
+        print(f"on_workspace_focus(i3, event):\n  {event}")
         #for window in event.current.leaves():
             #print(f"event:\n  {window.name}")
             #print(f"event:\n  {event}\n")
@@ -28,6 +28,7 @@ def on_workspace_focus(i3, event):
 
 def on_window(i3, event):
     print(f"on_window():\n  {event}\n")
+
 
 # Report event and tree information
 async def binding_report(i3, event):
@@ -109,10 +110,12 @@ async def on_window_focus(i3, event):
 
 async def main():
     i3 = await Connection(auto_reconnect=True).connect()
-    workspaces = await i3.get_workspaces()
+    #workspaces = await i3.get_workspaces()
 
     i3.on(Event.WINDOW_FOCUS, on_window_focus)
-    i3.on(Event.BINDING, binding_report)
+    i3.on(Event.BINDING, on_window_focus, binding_report)
+
+    #i3.on(Event.BINDING, on_window_focus)
     #i3.on(Event.WINDOW, on_window)
     #i3.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
 
